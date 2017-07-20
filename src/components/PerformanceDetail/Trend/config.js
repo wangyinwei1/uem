2ff1889bbbb3d6b075600fa17a4631542b06d38f
xpal7512 <943502449@qq.com>
2017-07-20 21:01:05 +0800

    增加性能详情页Trend组件

--- src/components/PerformanceDetail/Trend/config.js
+++ src/components/PerformanceDetail/Trend/config.js
@@ -8,6 +8,9 @@ export default Immutable.fromJS({
             type: 'category',
             data: []
         },
+        grid: [{
+            top: '25%'
+        }],
         yAxis: [{
             type: 'value',
             show: true,
@@ -25,12 +28,9 @@ export default Immutable.fromJS({
                     color: '#236592'
                 }
             },
-            // axisLine: {
-            //     show: false
-            // },
-            // axisTick: {
-            //     show: false
-            // }
+            axisTick: {
+                show: false
+            },
             show: true,
             name: '单位：次',
         }],
@@ -60,183 +60,168 @@ export default Immutable.fromJS({
             name: '客户端',
             type: 'bar',
             stack: '总量',
-            // data: []
-            data: _.range(Math.random() * 100, Math.random * 300)
+            data: []
+            // data: _.range(Math.random() * 100, Math.random * 300)
         }, {
             name: '网络传输',
             type: 'bar',
             stack: '总量',
-            // data: []
-            data: _.range(Math.random() * 100, Math.random * 300)
+            data: []
+            // data: _.range(Math.random() * 100, Math.random * 300)
         }, {
             name: '服务器',
             type: 'bar',
             stack: '总量',
-            // data: []
-            data: _.range(Math.random() * 100, Math.random * 300)
+            data: []
+            // data: _.range(Math.random() * 100, Math.random * 300)
         }, {
             name: '点击次数',
             type: 'line',
-            // data: []
-            data: _.range(Math.random() * 100, Math.random * 300)
+            smooth: true,
+            data: [],
+            yAxisIndex: 1,
+            // data: _.range(Math.random() * 100, Math.random * 300)
         }]
     },
-    thruput: {},
-    apdex: {},
-    throughput: {},
+    thruput: {
+        color: ['#90ec7d', '#03a9f3', 'rgb(254,226,92)'],
+        legend: {
+            itemWidth: 8,
+            itemHeight: 8,
+            data: [{
+                name: '中位数',
+                icon: 'circle'
+            }, {
+                name: '平均值',
+                icon: 'circle'
+            }, {
+                name: '最慢5%趋势',
+                icon: 'circle'
+            }],
+            top: 15,
+            right: 15,
+            textStyle: {
+                color: '#fff'
+            }
+        },
+        tooltip: {
+            // 悬浮弹窗的颜色
+            backgroundColor: 'rgba(50,50,50,0.7)',
+            textStyle: {
+                color: '#ffffff'
+            },
+            // 坐标轴指示器，坐标轴触发有效
+            trigger: 'axis',
+            axisPointer: {
+                // 默认为直线，可选为：'line' | 'shadow'          
+                type: 'shadow'
+            }
+        },
+        yAxis: [{
+            name: '单位：s',
+            //max:1,
+            min: 0,
+            minInterval: 0,
+            splitLine: {
+                show: true
+            },
+        }],
+        series: [{
+            name: '中位数',
+            type: 'line',
+            smooth: true,
+            data: []
+        }, {
+            name: '平均值',
+            type: 'line',
+            smooth: true,
+            data: []
+        }, {
+            name: '最慢5%趋势',
+            type: 'line',
+            smooth: true,
+            data: []
+        }]
+    },
+    apdex: {
+        yAxis: [{
+            name: '单位：次',
+            minInterval: 0,
+            min: 0,
+        }],
+        xAxis: {
+            type: 'category',
+            data: ['满意', '可接受', '不满意']
+        },
+        series: [{
+            name: '操作数',
+            barWidth: 50,
+            type: 'bar',
+            itemStyle: {
+                normal: {
+                    color: item => {
+                        const colors = ['#66dc6b', '#ffeb0b', '#ff5252'];
+                        return colors[item.dataIndex];
+                    }
+                }
+            },
+            data: []
+        }]
+    },
+    throughput: {
+        color: ['#90ED7D', 'rgb(254,226,92)'],
+        legend: {
+            itemWidth: 8,
+            itemHeight: 8,
+            data: [{
+                name: '吞吐率',
+                icon: 'rect'
+            }, {
+                name: '点击次数',
+                icon: 'circle'
+            }],
+            top: 15,
+            right: 15,
+            textStyle: {
+                color: '#fff'
+            }
+        },
+        grid: [{
+            top: '25%'
+        }],
+        yAxis: [{
+            name: '单位：rpm',
+            minInterval: 0,
+            min: 0,
+        }, {
+            name: '单位：次',
+            axisLine: {
+                show: false,
+                lineStyle: {
+                    color: '#70c3fb'
+                }
+            },
+            splitLine: {
+                lineStyle: {
+                    color: '#236592'
+                }
+            },
+            axisTick: {
+                show: false
+            },
+            minInterval: 1,
+            min: 0
+        }],
+        series: [{
+            name: '吞吐率',
+            type: 'bar',
+            data: []
+        }, {
+            name: '点击次数',
+            type: 'line',
+            smooth: true,
+            data: [],
+            yAxisIndex: 1,
+        }]
+    },
 })
-
-// export default Immutable.fromJS({
-//     default: {
-//         backgroundColor: 'transparent',
-//         title: {},
-//         xAxis: [{
-//             type: 'category',
-//             data: []
-//         }],
-//         yAxis: []
-//     },
-//     avgRspTime: {
-//         color: ['#6B7BFF', '#90ED7D', '#03B9FF', '#AADF2D'],
-//         title: {
-//             text: '',
-//             top: 'top'
-//         },
-//         tooltip: {
-//             // 悬浮弹窗的颜色
-//             backgroundColor: 'rgba(50,50,50,0.7)',
-//             textStyle: {
-//                 color: '#ffffff'
-//             },
-//             trigger: 'axis',
-//             axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-//                 type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-//             }
-//         },
-//         yAxis: [{
-//             name: '单位：s',
-//             //max:1,
-//             min: 0,
-//             minInterval: 1,
-//         }, {
-//             name: '单位：次',
-//             minInterval: 1,
-//             min: 0,
-//             max: 3,
-//             splitLine: {
-//                 show: true
-//             },
-//         }],
-//         series: [{
-//             name: '今日',
-//             type: 'line',
-//             symbol: 'circle',
-//             smooth: true,
-//             showSymbol: false,
-//             data: [1, 3, 4]
-//         }, {
-//             name: '昨日',
-//             type: 'bar',
-//             symbol: 'circle',
-//             smooth: true,
-//             showSymbol: false,
-//             data: [123, 12312, 415]
-//         }]
-//     },
-//     thruput: {
-//         color: ['#90ec7d', '#03a9f3', 'rgb(254,226,92)'],
-//         legend: {
-//             top: 3,
-//             right: 30,
-//             data: ['中位数', '平均值', '最慢5%趋势']
-//         },
-//         tooltip: {
-//             // 悬浮弹窗的颜色
-//             backgroundColor: 'rgba(50,50,50,0.7)',
-//             textStyle: {
-//                 color: '#ffffff'
-//             },
-//             trigger: 'axis',
-//             axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-//                 type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-//             }
-//         },
-//         title: {
-//             text: '',
-//             top: 'top'
-//         },
-//         yAxis: [{
-//             name: '单位：s',
-//             //max:1,
-//             min: 0,
-//             minInterval: 0,
-//             splitLine: {
-//                 show: true
-//             },
-//         }]
-//     },
-//     apdex: {
-//         title: {
-//             text: '',
-//             top: 'top'
-//         },
-//         tooltip: {
-//             // 悬浮弹窗的颜色
-//             backgroundColor: 'rgba(50,50,50,0.7)',
-//             textStyle: {
-//                 color: '#ffffff'
-//             },
-//             trigger: 'axis',
-//             axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-//                 type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-//             }
-//         },
-//         legend: {
-//             top: 3,
-//             right: 30,
-//             show: false,
-//             data: ['满意度']
-//         },
-//         yAxis: [{
-//             name: '单位：次',
-//             minInterval: 0,
-//             min: 0,
-
-//         }]
-//     },
-//     throughput: {
-//         color: ['#90ED7D', 'rgb(254,226,92)'],
-//         title: {
-//             text: '',
-//             top: 'top'
-//         },
-//         tooltip: {
-//             // 悬浮弹窗的颜色
-//             backgroundColor: 'rgba(50,50,50,0.7)',
-//             textStyle: {
-//                 color: '#ffffff'
-//             },
-//             trigger: 'axis',
-//             axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-//                 type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-//             }
-//         },
-//         legend: {
-//             top: 3,
-//             right: 30,
-//             data: ['吞吐率', '操作次数']
-//         },
-//         yAxis: [{
-//             name: '单位：rpm',
-//             minInterval: 0,
-//             min: 0,
-//         }, {
-//             name: '单位：次',
-//             minInterval: 1,
-//             min: 0,
-//             splitLine: {
-//                 show: true
-//             },
-//         }]
-//     },
-// })
--- src/components/PerformanceDetail/Trend/index.jsx
+++ src/components/PerformanceDetail/Trend/index.jsx
@@ -36,25 +36,62 @@ export default class Trend extends React.Component {
             case 0: return <BarChart
                 chartId={`trend-${itemId}-${activeTrend}`}
                 options={config.get('default').mergeDeep(config.get('avgRspTime'))
-                    .setIn(['xAxis', 'data'], trend.clientTime.map(item => moment(item.startTime).format('HH:mm')))
-                    //.setIn(['series', 0, 'data'], trend.clientTime.map(item => item.value))
-                    //.setIn(['series', 1, 'data'], trend.netTime.map(item => item.value))
-                    //.setIn(['series', 2, 'data'], trend.serverTime.map(item => item.value))
-                    //.setIn(['series', 3, 'data'], trend.clickNum.map(item => item.value))
+                    .setIn(['xAxis', 'data'], trend.clientTime.map(item => {
+                        const timeDiff = item.endTime - item.startTime;
+                        if (timeDiff <= 1800000) {
+                            return moment(item.startTime).format('HH:mm');
+                        } else {
+                            return moment(item.startTime).format('MM-DD HH:mm');
+                        }
+                    }))
+                    .setIn(['series', 0, 'data'], trend.clientTime.map(item => item.value))
+                    .setIn(['series', 1, 'data'], trend.netTime.map(item => item.value))
+                    .setIn(['series', 2, 'data'], trend.serverTime.map(item => item.value))
+                    .setIn(['series', 3, 'data'], trend.clickNum.map(item => item.value))
                     .toJS()}
             />;
 
             case 1: return <LineChart
                 chartId={`trend-${itemId}-${activeTrend}`}
-                options={config.get('default').mergeDeep(config.get('thruput')).toJS()}
+                options={config.get('default').mergeDeep(config.get('thruput'))
+                    .setIn(['xAxis', 'data'], trend.median.map(item => {
+                        const timeDiff = item.endTime - item.startTime;
+                        if (timeDiff <= 1800000) {
+                            return moment(item.startTime).format('HH:mm');
+                        } else {
+                            return moment(item.startTime).format('MM-DD HH:mm');
+                        }
+                    }))
+                    .setIn(['series', 0, 'data'], trend.median.map(item => item.value))
+                    .setIn(['series', 1, 'data'], trend.rspTime.map(item => item.value))
+                    .setIn(['series', 2, 'data'], trend.percent5.map(item => item.value))
+                    .toJS()}
             />;
-            case 2: return <BarChart
-                chartId={`trend-${itemId}-${activeTrend}`}
-                options={config.get('default').mergeDeep(config.get('apdex')).toJS()}
-            />;
+            case 2:
+                const apdexsArr = [];
+                apdexsArr.push(trend.apdexs['apdexS'] || 0);
+                apdexsArr.push(trend.apdexs['apdexT'] || 0);
+                apdexsArr.push(trend.apdexs['apdexD'] || 0);
+                return <BarChart
+                    chartId={`trend-${itemId}-${activeTrend}`}
+                    options={config.get('default').mergeDeep(config.get('apdex'))
+                        .setIn(['series', 0, 'data'], apdexsArr)
+                        .toJS()}
+                />;
             case 3: return <BarChart
                 chartId={`trend-${itemId}-${activeTrend}`}
-                options={config.get('default').mergeDeep(config.get('throughput')).toJS()}
+                options={config.get('default').mergeDeep(config.get('throughput'))
+                    .setIn(['xAxis', 'data'], trend.thruput.map(item => {
+                        const timeDiff = item.endTime - item.startTime;
+                        if (timeDiff <= 1800000) {
+                            return moment(item.startTime).format('HH:mm');
+                        } else {
+                            return moment(item.startTime).format('MM-DD HH:mm');
+                        }
+                    }))
+                    .setIn(['series', 0, 'data'], trend.thruput.map(item => item.value))
+                    .setIn(['series', 1, 'data'], trend.clickNum.map(item => item.value))
+                .toJS()}
             />;
         }
     }
