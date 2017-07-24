4dbacdfa0ca15a372b88d8b9544660090a5248d7
xpal7512 <943502449@qq.com>
2017-07-24 09:36:35 +0800

    修复Timing组件BUG

--- src/components/PerformanceDetail/Timing/index.jsx
+++ src/components/PerformanceDetail/Timing/index.jsx
@@ -35,6 +35,7 @@ export default class Timing extends React.Component {
     }
     componentDidMount() {
         this.items = $(this.refs.timing).find('dl').children('dd').children('span');
+        this.colWraps = $(this.refs.timing).find('dl').children('dd');
     }
     componentWillReceiveProps(nextProps) {
         const {
@@ -73,7 +74,13 @@ export default class Timing extends React.Component {
         this.items.map(item => {
             widths.push($(this.items[item]).width());
         });
-        const firstByteTimeWidth = widths[0] + widths[1] + widths[2] + widths[3] + 15;
+        const firstByteTimeWidth = (() => {
+            const colWrap_1 = widths[0] + widths[1] + widths[2];
+            if (colWrap_1 === 0) {
+                return this.colWraps.eq(1).width() + widths[3] + 15;
+            }
+            return colWrap_1 + widths[3] + 15;
+        })();
         const lastByteTimeWidth = firstByteTimeWidth + widths[4];
         const domLoadingTimeWidth = lastByteTimeWidth + widths[5] + 15;
         const pageAvgRspTimeWidth = domLoadingTimeWidth + widths[6];
